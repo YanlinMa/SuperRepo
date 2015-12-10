ef1e0021cd7dc9be84821df51b750b764e692c90
YanlinMa <yma3@stuy.edu>
2015-12-10 00:30:01 -0500

    files

---
+++ Hexadecimal.java
@@ -0,0 +1,188 @@
+//Team 1031/1225- Kate Johnston and Andrea Ma
+//APCS1 pd10
+//HW44- This or That or Fourteen Other Things
+//2015-12-08
+
+public class Hexadecimal {
+
+    // instance variables
+    
+    private int _decNum;
+    private String _hexNum;
+    private final static String HEXDIGITS = "0123456789ABCDEF";
+
+    // constructors
+
+    /*=====================================
+      default constructor
+      pre:  n/a
+      post: initializes _decNum to 0, _hexNum to "0"
+      =====================================*/
+    public Hexadecimal() { 
+        _decNum = 0;
+	_hexNum = "0";
+    }
+
+    /*=====================================
+      overloaded constructor
+      pre:  n >= 0
+      post: sets _decNum to n, _hexNum to equiv string of bits
+      =====================================*/
+    public Hexadecimal( int n ) {
+        _decNum = n;
+	_hexNum = decToHex(n);
+    }
+
+    /*=====================================
+      overloaded constructor
+      pre:  s is String representing non-negative hexadecimal number
+      post: sets _hexNum to input, _decNum to decimal equiv
+      =====================================*/
+    public Hexadecimal( String s ) {
+        _decNum = hexToDec(s);
+	_hexNum = s;
+    }
+
+    // methods
+    
+    /*=====================================
+      String toString() -- returns String representation of this Object
+      pre:  n/a
+      post: returns String of hexadecimal representing value of this Object
+      =====================================*/
+    public String toString() { 
+        return _hexNum;
+    }
+
+    /*=====================================
+      String decToHex(int) -- converts base-10 input to base-16
+      pre:  n >= 0
+      post: returns String of hexadecimal
+      =====================================*/
+    public static String decToHex( int n ) {
+	int r = 0;
+	String h = "";
+	while ( n != 0 ) {
+	    r = n % 16;
+	    n /= 16;
+	    h = HEXDIGITS.substring(r,r+1) + h;
+	}
+	return h;
+    }
+
+    /*=====================================
+      String decToHexR(int) -- converts base-10 input to hexadecimal, recursively
+      pre:  n >= 0
+      post: returns String of hexadecimal
+      =====================================*/
+    public static String decToHexR(int n){
+	return decToHexR(n,""); // helper method below
+    }
+    
+    public static String decToHexR(int n, String h) { 
+        if ( n == 0 ) {
+	    return h;
+	}
+        else {
+	    String b1 = ( Integer.parseInt(HEXDIGITS.substring(n,n+1)) % 16 ) + h;
+	    return decToHexR(n/16,b1);
+	}
+    }
+
+    /*=====================================
+      String hexToDec(String) -- converts base-10 input to hexadecimal
+      pre:  s represents non-negative hexadecimal number
+      post: returns decimal equivalent as int
+      =====================================*/
+    public static int hexToDec( String s ) {
+	String dec = "";
+	for (int i = 0 ; i < s.length() ; i++) {
+	    dec += HEXDIGITS.indexOf(s.substring(i, i + 1))
+		* Math.pow(16, s.length() - i - 1);
+	}
+	return Integer.valueOf(dec); 
+    }
+
+    /*=====================================
+      String hexToDecR(String) -- converts base-10 input to hexadecimal, recursively
+      pre:  s represents non-negative hexadecimal
+      post: returns decimal equivalent as int
+      =====================================*/
+    public static int hexToDecR( String s ) { 
+	if( s.length() == 1 ) {
+	    return HEXDIGITS.indexOf(s);
+	}
+	else {
+	    return hexToDecR( s.substring(1) )
+		+ HEXDIGITS.indexOf(s.substring(0,1))
+		* (int)Math.pow(16, s.length() - 1);
+	}
+    }
+
+    /*=============================================
+      boolean equals(Object) -- tells whether 2 Objs are equivalent
+      pre:  other is an instance of class Hexadecimal
+      post: Returns true if this and other are aliases (pointers to same 
+      Object), or if this and other represent equal values
+      =============================================*/
+    public boolean equals( Object other ) { 
+	Hexadecimal otherHex = (Hexadecimal)(other);
+	return this.compareTo(otherHex) == 0;
+    }
+
+    /*=============================================
+      int compareTo(Object) -- tells which of two Hexadecimal objects is greater
+      pre:  other is instance of class Hexadecimal
+      post: Returns 0 if this Object is equal to the input Object,
+      negative integer if this<input, positive integer otherwise
+      =============================================*/
+    public int compareTo( Object other ) {
+	Hexadecimal otherHex = (Hexadecimal)(other);
+	int x = this._decNum - otherHex._decNum;
+	if ( x > 0 ) {
+	    return 1;
+	}
+	else if ( x < 0 ) {
+	    return -1;
+	}
+	else {
+	    return x;
+	}
+    }
+
+    //main method for testing
+    public static void main( String[] args ) {
+
+	System.out.println();
+	System.out.println( "Testing ..." );
+
+	Hexadecimal b1 = new Hexadecimal(15);
+	Hexadecimal b2 = new Hexadecimal(15);
+	Hexadecimal b3 = b1;
+	Hexadecimal b4 = new Hexadecimal(7);
+
+	System.out.println( b1 );
+	System.out.println( b2 );
+	System.out.println( b3 );       
+	System.out.println( b4 );
+
+	System.out.println( "\n==..." );
+	System.out.println( b1 == b2 ); //should be false
+	System.out.println( b1 == b3 ); //should be true
+
+	System.out.println( "\n.equals()..." );
+	System.out.println( b1.equals(b2) ); //should be true
+	System.out.println( b1.equals(b3) ); //should be true
+	System.out.println( b3.equals(b1) ); //should be true
+	System.out.println( b4.equals(b2) ); //should be false
+	System.out.println( b1.equals(b4) ); //should be false
+
+	System.out.println( "\n.compareTo..." );
+	System.out.println( b1.compareTo(b2) ); //should be 0
+	System.out.println( b1.compareTo(b3) ); //should be 0
+	System.out.println( b1.compareTo(b4) ); //should be neg
+	System.out.println( b4.compareTo(b1) ); //should be pos
+	
+    } //end main()
+
+} //end class
---
+++ Rational.java
@@ -0,0 +1,165 @@
+public class Rational implements Comparable {
+
+    //Instance Variables
+    private int num;
+    private int den;
+
+    //Default Constructor
+    public Rational(){
+	num = 0;
+	den = 1;
+    }
+
+    //Overloaded Constructor
+    public Rational(int x, int y){
+	this();
+	if (y==0) {
+	    System.out.println("Error: undefined.");
+	}
+	else {    
+	    num = x;
+	    den = y;
+	}
+    }
+
+    //Methods
+    
+    //returns Rational as "a/b" (e.g. "1/2")
+    public String toString(){
+	return num + "/" + den;
+    }
+
+    //returns floating value of numerator (typecasted as double) divided by denominator
+    public double floatValue(){
+ 	double num2 = num;
+	return num2/den;
+    }
+
+    //multiplies numerators, multiplies denominators
+    public void multiply(Rational R){
+	num = num*R.num;
+	den = den*R.den;
+    }
+    
+    //divides numerators, divides denominators
+    public void divide(Rational R){
+	num = num/R.num;
+	den = den/R.den;
+    }
+
+    //PHASE II
+    
+    //returns gcd of numerator and denominator, Euclidean Algo
+    public  int gcd() {
+	int ret = 1; int a = num; int b = den;
+	if (a==b) {
+	    ret = a;
+	}
+	else {
+	    while (a!=0 && b!= 0) {
+		if (a>b) {
+		    a = a%b;
+		    ret= b;
+		}
+		else {
+		    b=b%a;
+		    ret =a;
+		}
+	    }
+	}
+	return ret;
+    }
+
+    //adds two rational objects
+    public void add(Rational R){
+	num = num*R.den+den*R.num;
+	den = den*R.den;
+    }
+
+    //substracts two rational objects
+    public void subtract(Rational R){
+	num = num*R.den-den*R.num;
+	den = den*R.den;
+    }
+
+    //reduces fraction, using gcd
+    public void reduce() {
+	int g = gcd();
+	num /= g;
+	den /= g;
+    }
+
+    //PHASE III
+
+    public static int gcd(int num, int den){
+	int ret = 1; 
+	if (num==den) {
+	    ret = num;
+	}
+	else {
+	    while (num!=0 && den!= 0) {
+		if (num>den) {
+		    num = num%den;
+		    ret= den;
+		}
+		else {
+		    den=den%num;
+		    ret =num;
+		}
+	    }
+	}
+	return ret;
+    }
+
+    public int compareTo(Rational R) {
+	Rational S = new Rational(num,den);
+	S.reduce();
+	R.reduce();
+	R.subtract(S);
+	return R.num;
+    }
+    
+    //PHASE IV
+
+    public boolean equals(Rational R){
+	if (num/den==R.num/R.den) {
+	    return true;
+	}
+	R.reduce();
+	if ((num/gcd(num,den))/(den/gcd(num,den))==(R.num/R.den)) {
+	    System.out.println(num/gcd(num,den));
+	    return true;
+	}
+	Rational S = new Rational(num, den);
+	if (S.equals(R)) {
+	    return true;
+	}
+	else {
+	    return false;
+	}
+    }
+
+    //Interface
+    public int compareTo(Object O){
+	Object P = new Rational(num, den);
+	P.reduce();
+	O.reduce();
+	O.subtract(P);
+	return O.num;
+    }
+
+    //main method -- Diagnostics
+    public static void main(String[] args){
+	Rational n = new Rational(1,3);
+	Rational m = new Rational(3,2);
+	Object l = new Rational(2,5);
+	n.multiply(m);
+	System.out.println(n);
+	n.reduce();
+	System.out.println(n);
+	System.out.println(n.compareTo(n));
+	System.out.println(n.compareTo(m));
+	System.out.println(m.compareTo(n));
+    }
+
+}//end of class Rational
